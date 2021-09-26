21667af2526ec199f95760922223ae96c022c30b
selina-22 <85050552+selina-22@users.noreply.github.com>
2021-09-26 20:31:45 +0530

    Add files via upload

---
+++ Bits-n-Bites---Cake-shop-Website-main/README.md
@@ -0,0 +1,6 @@
+# Bits-n-Bites---Cake-shop-Website
+![](img1.jpg)
+![](img2.jpg)
+![](img3.jpg)
+![](img4.jpg)
+![](img5.jpg)
---
+++ Bits-n-Bites---Cake-shop-Website-main/connectionf.php
@@ -0,0 +1,14 @@
+<?php
+	$name = "";
+	$email = "";
+	$msg = "";
+	$error = array();	
+	$con = mysqli_connect('sql305.epizy.com','epiz_27284877','duFJuG6d3F','epiz_27284877_BitsnBites');
+
+	if($con){
+			// echo "<script>alert('Congratulation Connected to MySQL (For SUBS).')</script>";
+		}else{
+			echo "<script>alert('xx MySQL Connection Error. xx')</script>";
+		}
+
+?>
